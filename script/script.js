b1f242754c298ba49f8346c8fda976415ea9bfb0
stefaneduard-deaconu <stefaneduard.deaconu@gmail.com>
2019-06-16 22:15:02 +0300

    set up audio samples, labels, and button and default sound

--- script/script.js
+++ script/script.js
@@ -1,26 +1,17 @@
 function set(option) {
 	var options = [
-		"animals",
-		"birds",
-		"clothing",
-		"car",
-		"diseases",
-		"education"
+		"_industrial",
+		"_oildrum",
+		"_ride",
+		"_rock"
 		]
 	var labels = [
-		"Animals",
-		"Birds",
-		"Clothing",
-		"Car",
-		"Diseases",
-		"Education"
+		"Industrial Beat",
+		"Oil Drums",
+		"Ride Patterns",
+		"Rock Sounds"
 		]
-	//console.log(document.getElementById("audio_player"));
-	
-	console.log(document.getElementsByTagName("h2")[0].innerHTML);
-	console.log(options[option - 1]);
 	document.getElementsByTagName("h2")[0].innerHTML = labels[option - 1];
-	console.log(document.getElementsByTagName("source")[0]);
 	document.getElementsByTagName("source")[0].src = `./audio/${options[option - 1]}.wav`;
 	var player = document.getElementById("audio_player");
 	player.load();
