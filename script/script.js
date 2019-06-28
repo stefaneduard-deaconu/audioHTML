0403e0b52f30949419de989ba10e106d499dc151
stefaneduard-deaconu <stefaneduard.deaconu@gmail.com>
2019-06-28 20:48:03 +0300

    more generic approach; the index is still hard-coded

--- script/script.js
+++ script/script.js
@@ -1,18 +1,30 @@
-function set(option) {
-	var options = [
+var options = [
 		"_industrial",
 		"_oildrum",
 		"_ride",
 		"_rock"
 		]
-	var labels = [
-		"Industrial Beat",
-		"Oil Drums",
-		"Ride Patterns",
-		"Rock Sounds"
-		]
-	document.getElementsByTagName("h2")[0].innerHTML = labels[option - 1];
-	document.getElementsByTagName("source")[0].src = `./audio/${options[option - 1]}.wav`;
+var labels = { }
+
+// we use this next function to create labels for each file
+// but we will create labels as they become necessary
+function toLabel(string) {
+		string = string.replace('_', ' ')
+					   .trim();
+		firstLetterToUpper = string.substr(0, 1) // getting the first letter
+							       .toUpperCase(); // making it uppercase
+		remainder = string.substr(1); // getting the remainder of the string
+		return firstLetterToUpper + remainder;
+	}
+
+function set(option) { // option is a string, that stores the name of the audio file
+	console.log(labels[option]); //
+	if (labels[option] == undefined)
+		labels[option] = toLabel(option);
+	console.log(labels[option]);
+	//
+	document.getElementsByTagName("h2")[0].innerHTML = labels[option];
+	document.getElementsByTagName("source")[0].src = `./audio/${option}.wav`;
 	var player = document.getElementById("audio_player");
 	player.load();
 }
